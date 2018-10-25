ccc7ec2b639ea1a278f63f48dd191436be331871
Nicolò Pratelli <pratelli1991@gmail.com>
2018-10-25 15:10:08 +0200

    renewed fb buttons

--- Assets/PHP/OggiSTI_event.php
+++ Assets/PHP/OggiSTI_event.php
@@ -227,13 +227,13 @@ documenti documents cignoni giovanni pratelli nicolò oggi almanacco oggisti" />
         <tr><td>Facebook:</td><td>
         <?php 
             if($fb==0){ 
-                echo "Non pubblicabile";
+                echo '<button class="btn btn-danger" disabled> Non pubblicabile </button>';
                 if($stato=="Pubblicato"){
                     echo '<button type = "submit" name = "facebookOn" id = "facebookOn" class="btn btn-success btn-circle"> ON </button>';
                     echo '<button type = "submit" name = "facebookOff" id = "facebookOff" class="btn btn-danger btn-circle" disabled> OFF  </button>';
                 }
             }else{
-                echo "Pubblicabile";
+                echo '<button class="btn btn-success" disabled> Pubblicabile </button>';
                 if($stato=="Pubblicato"){
                     echo '<button type = "submit" name = "facebookOn" id = "facebookOn" class="btn btn-success btn-circle" disabled> ON </button>';
                     echo '<button type = "submit" name = "facebookOff" id = "facebookOff" class="btn btn-danger btn-circle"> OFF </button>';
